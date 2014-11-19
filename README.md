2302cd02e1b2e2c92d60a30f5c70c9c5f983ef2d
Stéphane Turquay <stephane@toopixel.ch>
2014-11-19 15:21:14 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-htaccess-htpasswd-generator
-===========================
+htaccess & htpasswd generator
+===================
 
-This script allows you to generate .htaccess &amp; .htpasswd files easily.
+This script allows you to generate htaccess & htpasswd files easily.
+
+Installation
+----
+
+* Copy file access.php to the directory you want to lock
+* Open access.php hosted on the server in your internet browser
+* Write your username and password
+* You're done. access.php is now deleted.
---
+++ access.php
@@ -0,0 +1,67 @@
+<?php 
+
+if (!empty($_POST['username']) && !empty($_POST['password'])) {
+	
+	$username = filter_var(trim($_POST['username']), FILTER_SANITIZE_STRING);
+	$password = filter_var(trim($_POST['password']), FILTER_SANITIZE_STRING);
+	$password = crypt($password, base64_encode($password));
+	$password = $username . ':' . $password;
+
+
+	$htaccess_content = '
+AuthName "Protected Access"
+AuthType Basic
+AuthUserFile "' . dirname(__FILE__) . '.htpasswd"
+Require valid-user';
+		
+	if (file_exists('.htaccess')) {
+	
+		$htaccess = file_get_contents('.htaccess');
+	
+		if (strpos($htaccess,'AuthUserFile') === false) {
+		
+			file_put_contents('.htaccess', $htaccess_content, FILE_APPEND);
+			
+		}
+		
+	} else {
+	
+		file_put_contents('.htaccess', $htaccess_content, FILE_APPEND);
+	}
+
+	if (file_exists('.htpasswd')) {
+	
+		$htpasswd = file_get_contents('.htpasswd');
+		
+		if (strpos($htpasswd, $username) === false) {
+	    	
+	    	file_put_contents('.htpasswd', ' 
+'. $password, FILE_APPEND);
+		}
+	
+	} else {
+		
+		file_put_contents('.htpasswd', $password, FILE_APPEND);
+	}
+
+	unlink(__FILE__);
+
+}
+else {
+
+?>
+<html>
+	<head>
+		<title>.htaccess Generator</title>
+	</head>
+	<body>
+		<form method="post">
+			<input type="text" name="username" placeholder="Username" />
+			<input type="password" name="password" placeholder="Password" />
+			<input type="submit" value="Generate" />
+		</form>
+	</body>
+</html>
+<?php
+}
+?>
